cb7c9fa0ed1303aad536a3983d9c7adee8bbb15f
Dobrota <dobrotakevin@yahoo.fr>
2020-11-17 17:09:06 +0100

    HookDiscord test
    hook non fonctionnel pour le moment

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 let disconnectionBtn = document.getElementById('disconnectionBtn')
 disconnectionBtn.addEventListener('click', () => {
     localStorage.setItem('connected', false)
-    window.location.href = '../login.html'
+    window.location.href = '../index.html'
 })
+const whurl = "https://discord.com/api/webhooks/778269268533444658/DqY1ieHcWbGCG6LEjzoGUDNXSIyffvc21zViIofbm0vson_v2rtuFrjUrTqYt6LNG3H8";
 
 function sendMessage() {
     let txt = document.getElementById("msg").value;
+    let msg = {"content": txt};
+    console.log(msg)
+    fetch(whurl + "?wait=true",{
+        method:"POST",
+        headers: {"constent-type":"application/json"
+        }, 
+        "body":JSON.stringify(msg)})
+        .then(a=>a.json()).then(console.log)  
     console.log(txt);
 }
