1fa0b0b54a3eb69e785c16266345f0b78e48e680
Dobrota <dobrotakevin@yahoo.fr>
2020-11-17 15:18:45 +0100

    BDD
    mongodb,Node.Js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    TOKEN: "token",
+    PREFIX: "c?",
+    DBCONNECTION: "mongodb+srv://Ugo_P:codringctropbien#2020@codringcluster.zarcm.mongodb.net/register.js?retryWrites=true&w=majority",
+}
