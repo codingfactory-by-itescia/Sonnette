c63acf1740d8b699c01a4f7d1056b575e65b2669
Léo <leo.gregori95@gmail.com>
2021-01-06 15:46:46 +0100

    Add synthese vocal

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ function validTextArea() {
         return false
     }
 }
+
 function validSound() {
     event.preventDefault()
     form.style.transform = "translateX(-1000px)"
@@ -29,6 +30,7 @@ function playSound(n) {
 
 const urlDis = "https://discord.com/api/webhooks/778269268533444658/DqY1ieHcWbGCG6LEjzoGUDNXSIyffvc21zViIofbm0vson_v2rtuFrjUrTqYt6LNG3H8";
 
+let durationMsg;
 function sendMessage() {
     event.preventDefault()
     let txt = document.getElementById("msgArea").value;
@@ -44,7 +46,42 @@ function sendMessage() {
         "body":JSON.stringify(msg)})
         .then(a=>a.json()).then(console.log)
     console.log(txt);
-    setTimeout(() => {
-      window.location.reload()
-    }, 500)
+    let toSpeak = new SpeechSynthesisUtterance(textArea.value);
+    let selectedVoiceName = voiceList.selectedOptions[0].getAttribute('data-name');
+    voices.forEach((voice)=>{
+        if(voice.name === selectedVoiceName){
+            toSpeak.voice = voice;
+        }
+    });
+    synth.speak(toSpeak);
+    toSpeak.onend = function(event){
+        durationMsg = event.elapsedTime
+        console.log(durationMsg)
+        window.location.reload()
+    }
+    
+}
+
+let voiceList = document.querySelector('#voiceList');
+let synth = window.speechSynthesis;
+let voices = [];
+
+PopulateVoices();
+if(speechSynthesis !== undefined){
+    speechSynthesis.onvoiceschanged = PopulateVoices;
+}
+
+function PopulateVoices(){
+    voices = synth.getVoices();
+    let selectedIndex = voiceList.selectedIndex < 0 ? 0 : voiceList.selectedIndex;
+    voiceList.innerHTML = '';
+    voices.forEach((voice)=>{
+        let listItem = document.createElement('option');
+        listItem.textContent = voice.name;
+        listItem.setAttribute('data-lang', voice.lang);
+        listItem.setAttribute('data-name', voice.name);
+        voiceList.appendChild(listItem);
+    });
+
+    voiceList.selectedIndex = selectedIndex;
 }
