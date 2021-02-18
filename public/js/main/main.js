aa23406204e5343422e4eb912bf5cf16bdff53bf
Maxime Luchini <Maxime.luchini@edu.itescia.fr>
2021-02-18 16:28:53 +0100

    Alert Update
    when you click on the "click me" button selected alert was running

--- public/js/main/main.js
+++ public/js/main/main.js
@@ -33,6 +33,109 @@ function playSound(n) {
   sound.volume = .5
 }
 
+let checkSound1 = document.getElementById("sound1")
+let checkSound2 = document.getElementById("sound2")
+let checkSound3 = document.getElementById("sound3")
+let checkSound4 = document.getElementById("sound4")
+let checkSound5 = document.getElementById("sound5")
+let checkSound6 = document.getElementById("sound6")
+let checkSound7 = document.getElementById("sound7")
+
+let msgButton= document.querySelector('.sendMsgBtn')
+
+function check1(){
+    checkSound1.checked = true;
+    checkSound2.checked = false;
+    checkSound3.checked = false;
+    checkSound4.checked = false;
+    checkSound5.checked = false;
+    checkSound6.checked = false;
+    checkSound7.checked = false;
+    console.log('check1')
+}
+function check2(){
+    checkSound1.checked = false;
+    checkSound2.checked = true;
+    checkSound3.checked = false;
+    checkSound4.checked = false;
+    checkSound5.checked = false;
+    checkSound6.checked = false;
+    checkSound7.checked = false;
+}
+function check3(){
+    checkSound1.checked = false;
+    checkSound2.checked = false;
+    checkSound3.checked = true;
+    checkSound4.checked = false;
+    checkSound5.checked = false;
+    checkSound6.checked = false;
+    checkSound7.checked = false;
+}
+function check4(){
+    checkSound1.checked = false;
+    checkSound2.checked = false;
+    checkSound3.checked = false;
+    checkSound4.checked = true;
+    checkSound5.checked = false;
+    checkSound6.checked = false;
+    checkSound7.checked = false;
+}
+function check5(){
+    checkSound1.checked = false;
+    checkSound2.checked = false;
+    checkSound3.checked = false;
+    checkSound4.checked = false;
+    checkSound5.checked = true;
+    checkSound6.checked = false;
+    checkSound7.checked = false;
+}
+function check6(){
+    checkSound1.checked = false;
+    checkSound2.checked = false;
+    checkSound3.checked = false;
+    checkSound4.checked = false;
+    checkSound5.checked = false;
+    checkSound6.checked = true;
+    checkSound7.checked = false;
+}
+function check7(){
+    checkSound1.checked = false;
+    checkSound2.checked = false;
+    checkSound3.checked = false;
+    checkSound4.checked = false;
+    checkSound5.checked = false;
+    checkSound6.checked = false;
+    checkSound7.checked = true;
+}
+
+function checkChecked(){
+    if (checkSound1.checked = true){
+        let audio = new Audio('../audio/sound1.mp3')
+        audio.play()
+        sound.volume = .5
+    }else if (checkSound2.checked = true){
+        let audio = new Audio('../audio/sound2.mp3')
+        audio.play()
+        sound.volume = .5
+    }else if (checkSound3.checked = true){
+        let audio = new Audio('../audio/sound3.mp3')
+        audio.play()   
+        sound.volume = .5
+    }else if (checkSound4.checked = true){
+        let audio = new Audio('../audio/sound4.mp3')
+        audio.play()    
+        sound.volume = .5
+    }else if (checkSound5.checked = true){
+        let audio = new Audio('../audio/sound5.mp3')
+        audio.play()  
+        sound.volume = .5  
+    }else if (checkSound6.checked = true){
+        let audio = new Audio('../audio/sound6.mp3')
+        audio.play() 
+        sound.volume = .5
+    }       
+}
+
 const urlDis = "https://discord.com/api/webhooks/778269268533444658/DqY1ieHcWbGCG6LEjzoGUDNXSIyffvc21zViIofbm0vson_v2rtuFrjUrTqYt6LNG3H8";
 
 let durationMsg;
@@ -67,6 +170,8 @@ async function sendMessage() {
         textArea.value = ''
         form.style.transform = 'translate(0)'
     }
+    checkChecked()    
+    
 }
 
 let voiceList = document.querySelector('#voiceList');
