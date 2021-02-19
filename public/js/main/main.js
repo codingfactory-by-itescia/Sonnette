f3cda5afb8fcab11ef4d0c9f6c1d71b9b3a52624
Maxime Luchini <Maxime.luchini@edu.itescia.fr>
2021-02-19 11:21:44 +0100

    import alert

--- public/js/main/main.js
+++ public/js/main/main.js
@@ -45,7 +45,6 @@ function check1(){
     checkSound5.checked = false;
     checkSound6.checked = false;
     checkSound7.checked = false;
-    console.log('check1')
 }
 function check2(){
     checkSound1.checked = false;
@@ -130,6 +129,75 @@ function checkChecked(){
     }       
 }
 
+let input = document.querySelector('#input');
+var preview = document.querySelector('.preview');
+
+
+input.addEventListener('change', updateImageDisplay);
+
+function updateImageDisplay() {
+    while(preview.firstChild) {
+      preview.removeChild(preview.firstChild);
+    }
+  
+    var curFiles = input.files;
+    if(curFiles.length === 0) {
+      var para = document.createElement('p');
+      para.textContent = 'No files currently selected for upload';
+      preview.appendChild(para);
+    } else {
+      var list = document.createElement('ol');
+      preview.appendChild(list);
+      for(var i = 0; i < curFiles.length; i++) {
+        var listItem = document.createElement('li');
+        var para = document.createElement('p');
+        if(validFileType(curFiles[i])) {
+          para.textContent = 'File name ' + curFiles[i].name + ', file size ' + returnFileSize(curFiles[i].size) + '.';
+          var image = document.createElement('audio');
+          image.src = window.URL.createObjectURL(curFiles[i]);
+  
+          listItem.appendChild(image);
+          listItem.appendChild(para);
+  
+        } else {
+          para.textContent = 'File name ' + curFiles[i].name + ': Not a valid file type. Update your selection.';
+          listItem.appendChild(para);
+        }
+  
+        list.appendChild(listItem);
+      }
+    }
+  }
+
+  var fileTypes = [
+    'audio/mp3',
+    'audio/mpeg'
+
+  ]
+  
+  function validFileType(file) {
+    for(var i = 0; i < fileTypes.length; i++) {
+        console.log(file.type)
+      if(file.type === fileTypes[i]) {
+        return true;
+      }
+    }
+  
+    return false;
+  }
+
+  function returnFileSize(number) {
+    if(number < 1024) {
+      return number + ' octets';
+    } else if(number >= 1024 && number < 1048576) {
+      return (number/1024).toFixed(1) + ' Ko';
+    } else if(number >= 1048576) {
+      return (number/1048576).toFixed(1) + ' Mo';
+    }
+  }
+
+
+
 const urlDis = "https://discord.com/api/webhooks/778269268533444658/DqY1ieHcWbGCG6LEjzoGUDNXSIyffvc21zViIofbm0vson_v2rtuFrjUrTqYt6LNG3H8";
 
 let durationMsg;
@@ -157,14 +225,17 @@ async function sendMessage() {
             toSpeak.voice = voice;
         }
     });
-    synth.speak(toSpeak);
-    toSpeak.onend = function(event){
-        durationMsg = event.elapsedTime
-        console.log(durationMsg)
-        textArea.value = ''
-        form.style.transform = 'translate(0)'
-    }
-    checkChecked()    
+
+    checkChecked() 
+
+    setTimeout(function(){synth.speak(toSpeak);
+        toSpeak.onend = function(event){
+            durationMsg = event.elapsedTime
+            console.log(durationMsg)
+            textArea.value = ''
+            form.style.transform = 'translate(0)'
+        }; 
+    }, 7000);
     
 }
 
