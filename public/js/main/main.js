82c59ad6c1ca3440d3a7cb604b280d2fada4efa7
Labyrinthegreg <leo.gregori95@gmail.com>
2021-03-24 14:08:22 +0100

    sound import and timeout before speak

--- public/js/main/main.js
+++ public/js/main/main.js
@@ -23,11 +23,16 @@ function validSound() {
     form.style.transform = "translateX(-1000px)"
 }
 
-function playSound(n) {
+async function playSound(n) {
     // Play the sounds selected
-  let sound = new Audio('../audio/sound' + n + '.mp3')
-  sound.play()
-  sound.volume = .5
+    if (n == 7) {
+        let sound = new Audio(urlCurrentSound)
+        await getDuration(sound)
+    } else {
+        let sound = new Audio('../audio/sound' + n + '.mp3') 
+        await getDuration(sound)
+    }
+    
 }
 
 // Check wich sound is selected
@@ -105,102 +110,85 @@ function check7(){
     checkSound7.checked = true;
 }
 
-function checkChecked(){
+async function checkChecked(toSpeak){
+    let audio
     if (checkSound1.checked ==true){
-        let audio = new Audio('../audio/sound1.mp3')
-        audio.play()
-        audio.volume = .2
+        audio = new Audio('../audio/sound1.mp3')
     }else if (checkSound2.checked == true){
-        let audio = new Audio('../audio/sound2.mp3')
-        audio.play()
-        audio.volume = .2
+        audio = new Audio('../audio/sound2.mp3')
     }else if (checkSound3.checked == true){
-        let audio = new Audio('../audio/sound3.mp3')
-        audio.play()   
-        audio.volume = .2
+        audio = new Audio('../audio/sound3.mp3')
     }else if (checkSound4.checked == true){
-        let audio = new Audio('../audio/sound4.mp3')
-        audio.play()    
-        audio.volume = .2
+        audio = new Audio('../audio/sound4.mp3')
     }else if (checkSound5.checked == true){
-        let audio = new Audio('../audio/sound5.mp3')
-        audio.play()  
-        audio.volume = .2
+        audio = new Audio('../audio/sound5.mp3')
     }else if (checkSound6.checked == true){
-        let audio = new Audio('../audio/sound6.mp3')
-        audio.play() 
-        audio.volume = .2
-    }       
+        audio = new Audio('../audio/sound6.mp3')
+    } else if (checkSound7.checked == true){
+        audio = new Audio(urlCurrentSound)
+    }
+    await getDurationAndSpeak(audio, toSpeak)    
+}
+
+async function getDuration(audio) {
+    $(audio).on("loadedmetadata", async function(){
+        let durationSound = audio.duration;
+        await audio.play()
+        audio.volume = .25
+        if (durationSound < 8) {
+            setTimeout(function(){
+                pauseAudio(audio)
+            }, durationSound*1000)
+        } else {
+            setTimeout(function(){
+                pauseAudio(audio)
+            }, 7000)
+        }
+    });
+}
+
+async function getDurationAndSpeak(audio, toSpeak) {
+    $(audio).on("loadedmetadata", async function(){
+        await audio.play()
+        audio.volume = .25
+        let durationSound = audio.duration
+        if (durationSound < 8) {
+            setTimeout(function(){
+                pauseAudio(audio)
+                SpeakWithTimeOut(toSpeak)
+            }, durationSound*1000)
+        } else {
+            setTimeout(function(){
+                pauseAudio(audio)
+                SpeakWithTimeOut(toSpeak)
+            }, 7000)
+        }
+        
+    })
+    
+}
+
+function pauseAudio(x) {
+    x.pause();
 }
 
 let input = document.querySelector('#input');
-var preview = document.querySelector('.preview');
+let para = document.querySelector('.preview p');
+let urlCurrentSound;
 
 
 input.addEventListener('change', updateImageDisplay);
 
 function updateImageDisplay() {
-    while(preview.firstChild) {
-      preview.removeChild(preview.firstChild);
-    }
-  
-    var curFiles = input.files;
-    if(curFiles.length === 0) {
-      var para = document.createElement('p');
-      para.textContent = 'No files currently selected for upload';
-      preview.appendChild(para);
+    var curFiles = input.files;    
+
+    if(curFiles.length == 0) {
+      para.innerHTML = 'Pas de fichier importé';
     } else {
-      var list = document.createElement('ol');
-      preview.appendChild(list);
-      for(var i = 0; i < curFiles.length; i++) {
-        var listItem = document.createElement('li');
-        var para = document.createElement('p');
-        if(validFileType(curFiles[i])) {
-          para.textContent = 'File name ' + curFiles[i].name + ', file size ' + returnFileSize(curFiles[i].size) + '.';
-          var image = document.createElement('audio');
-          image.src = window.URL.createObjectURL(curFiles[i]);
-  
-          listItem.appendChild(image);
-          listItem.appendChild(para);
-  
-        } else {
-          para.textContent = 'File name ' + curFiles[i].name + ': Not a valid file type. Update your selection.';
-          listItem.appendChild(para);
-        }
-  
-        list.appendChild(listItem);
-      }
+        para.innerHTML = 'Nom du son importé ' + curFiles[0].name ;
+        urlCurrentSound = URL.createObjectURL(curFiles[0])
     }
-  }
-
-  var fileTypes = [
-    'audio/mp3',
-    'audio/mpeg'
-
-  ]
-  
-  function validFileType(file) {
-    for(var i = 0; i < fileTypes.length; i++) {
-        console.log(file.type)
-      if(file.type === fileTypes[i]) {
-        return true;
-      }
-    }
-  
-    return false;
-  }
-
-  function returnFileSize(number) {
-    if(number < 1024) {
-      return number + ' octets';
-    } else if(number >= 1024 && number < 1048576) {
-      return (number/1024).toFixed(1) + ' Ko';
-    } else if(number >= 1048576) {
-      return (number/1048576).toFixed(1) + ' Mo';
-    }
-  }
-
-
+}
 
 const urlDis = "https://discord.com/api/webhooks/778269268533444658/DqY1ieHcWbGCG6LEjzoGUDNXSIyffvc21zViIofbm0vson_v2rtuFrjUrTqYt6LNG3H8";
 
@@ -231,15 +219,16 @@ async function sendMessage() {
         }
     });
 
-    checkChecked() 
+    await checkChecked(toSpeak) 
+}
 
-    setTimeout(function(){synth.speak(toSpeak);
-        toSpeak.onend = function(event){
-            durationMsg = event.elapsedTime
-            textArea.value = ''
-            form.style.transform = 'translate(0)'
-        };
-    }, 5000);
+function SpeakWithTimeOut(toSpeak){
+    synth.speak(toSpeak);
+    toSpeak.onend = function(event){
+        durationMsg = event.elapsedTime
+        textArea.value = ''
+        form.style.transform = 'translate(0)'
+    }       
 }
 
 let voiceList = document.querySelector('#voiceList');
@@ -267,6 +256,7 @@ function PopulateVoices(){
 }
 
 async function checkForRefresh(){
+    // Check if ther is the same number of cards in the site that there is in the bdd
     counter = 0;
     allMsgData = await getAllMessages();
     lastsMsg = $('.msgCard');
