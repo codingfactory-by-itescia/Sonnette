9bd40e09b934e496d33e5889862a1dbc3e7a4420
Dobrota <dobrotakevin@yahoo.fr>
2021-03-26 14:50:38 +0100

    fix token slack

--- private/server.js
+++ private/server.js
@@ -6,7 +6,7 @@ const { WebClient } = require('@slack/web-api');
 require = require("esm")(module/*,options*/)
 
 // Slack initiation 
-const SLACK_OAUTH_TOKEN = 'xoxb-1780620095984-1780640389808-Jn70etx0LyToh1eoPOZODlXp'
+const SLACK_OAUTH_TOKEN = 'xoxb-1780620095984-1780640389808-wguKG7GK69IF0FqaWP3gl5X8'
 const BOT_SPAM_CHANNEL = 'C01MU05PUSK' // this is the channel you want your bot online & spam to go
 const web = new WebClient(SLACK_OAUTH_TOKEN);
 
