a05f019897edfa211cde920fc2bc8d5223959334
Dobrota <dobrotakevin@yahoo.fr>
2021-03-26 13:10:43 +0100

    Auto stash before merge of "backend" and "origin/backend"

--- private/src_Slack/constants.js
+++ private/src_Slack/constants.js
@@ -1,2 +1,2 @@
-export const SLACK_OAUTH_TOKEN = 'xoxb-1507173106646-1526569773841-9Di9v2zHzorCqUZuetuqhW7O'
-export const BOT_SPAM_CHANNEL = 'C01FGFS3317' // this is the channel you want your bot online & spam to go
+export const SLACK_OAUTH_TOKEN = 'xoxb-1780620095984-1780640389808-dNJqyEKZMvn4M0xONs1mX1bp'
+export const BOT_SPAM_CHANNEL = 'C01MU05PUSK' // this is the channel you want your bot online & spam to go
